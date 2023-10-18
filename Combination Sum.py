686c5262d2059bd76a0fb74586b2f18cfb605898
Eswar <manikantapadala358@gmail.com>
2023-10-18 21:51:11 +0530

    10/18/2023

---
+++ Combination Sum.py
@@ -0,0 +1,41 @@
+#https://leetcode.com/problems/combination-sum/description/
+"""Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+frequency
+ of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations."""
+
+
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        def backtrack(start, target, current_combination):
+            if target == 0:
+                result.append(current_combination[:])
+                return
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue  # Skip duplicates to ensure uniqueness
+                if candidates[i] <= target:
+                    current_combination.append(candidates[i])
+                    backtrack(i, target - candidates[i], current_combination)
+                    current_combination.pop()
+
+        candidates.sort()  # Sort the candidates to handle duplicates
+        result = []
+        backtrack(0, target, [])
+        return result
+        
+        
