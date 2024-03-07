5577a2dfac3ff0649e8fbf49a97688f2ac66f3dd
Eswar <manikantapadala358@gmail.com>
2024-03-07 23:52:56 +0530

    3/7/24

--- Longest Continuous Increasing Subsequence.py
+++ Longest Continuous Increasing Subsequence.py
@@ -4,14 +4,22 @@ Given an unsorted array of integers nums, return the length of the longest conti
 
 A continuous increasing subsequence is defined by two indices l and r (l < r) such that it is [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] and for each l <= i < r, nums[i] < nums[i + 1].
 """
-class solution(object):
+class Solution(object):
     def findLengthOfLCIS(self,nums):
-        ans=1
-        count=1
-        for i in range( 1,len(nums)):
-            if nums[i]>nums[i-1]:
-                count+=1
-                ans=max(ans,count)
+        if not nums:
+            return 0
+
+        max_length = 1
+        current_length = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                current_length += 1
+                max_length = max(max_length, current_length)
             else:
-                count=1
-        return count
+                current_length = 1
+
+        return max_length
+
+
+        
